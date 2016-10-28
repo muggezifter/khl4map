6106423c28dd9706b09347950229fb8743026921
muggezifter <muggezifter@gmail.com>
2016-10-28 15:12:13 +0200

    first attempt at installer

--- install.js
+++ install.js
@@ -1,1 +1,20 @@
-console.log(__dirname);
+var fs = require('fs');
+
+function copyFile(source, target) {
+    return new Promise(function(resolve, reject) {
+        var rd = fs.createReadStream(source);
+        rd.on('error', rejectCleanup);
+        var wr = fs.createWriteStream(target);
+        wr.on('error', rejectCleanup);
+        function rejectCleanup(err) {
+            rd.destroy();
+            wr.end();
+            reject(err);
+        }
+        wr.on('finish', resolve);
+        rd.pipe(wr);
+    });
+}
+
+copyFile('./dist/index.html','../../public/index.html');
+console.log('copied index.html');
